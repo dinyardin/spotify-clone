758a1f3fc0fa9e41fd0dd72e461263bf78e4328a
dinyardin <dinyardin@gmail.com>
2021-07-13 14:09:30 -0400

    Added preliminary styling on Footer component

--- src/components/Player/Footer.tsx
+++ src/components/Player/Footer.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import './Footer.css'
 
 function Footer() {
-    return <div className="footer">Footer</div>
+    return (
+        <div className="footer">
+            <div className="footer__left">{/*  Album and song details */}1</div>
+            <div className="footer__center">{/* Player controller */}2</div>
+            <div className="footer__right">{/* Volume controller */}3</div>
+        </div>
+    )
 }
 
 export default Footer
