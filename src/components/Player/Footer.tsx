0f6d0b71e56f35544f729c1f17e4a471d47dcfa0
dinyardin <dinyardin@gmail.com>
2021-07-13 14:18:54 -0400

    Added the center section of the Footer with styling

--- src/components/Player/Footer.tsx
+++ src/components/Player/Footer.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './Footer.css'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline'
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious'
+import SkipNextIcon from '@material-ui/icons/SkipNext'
+import ShuffleIcon from '@material-ui/icons/Shuffle'
+import RepeatIcon from '@material-ui/icons/Repeat'
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline'
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer__left">{/*  Album and song details */}1</div>
-            <div className="footer__center">{/* Player controller */}2</div>
+            <div className="footer__center">
+                {/* Player controller */}
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PauseCircleOutlineIcon className="footer__icon" />
+                <PlayCircleOutlineIcon className="footer__icon" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+            </div>
             <div className="footer__right">{/* Volume controller */}3</div>
         </div>
     )
