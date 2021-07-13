afadaf882c9b19da75ce59ac62a270e14312a9e4
dinyardin <dinyardin@gmail.com>
2021-07-13 14:22:57 -0400

    Added the right section of the Footer component

--- src/components/Player/Footer.tsx
+++ src/components/Player/Footer.tsx
@@ -6,6 +6,9 @@ import SkipNextIcon from '@material-ui/icons/SkipNext'
 import ShuffleIcon from '@material-ui/icons/Shuffle'
 import RepeatIcon from '@material-ui/icons/Repeat'
 import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline'
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay'
+import VolumeDownIcon from '@material-ui/icons/VolumeDown'
+import { Grid, Slider } from '@material-ui/core'
 
 function Footer() {
     return (
@@ -20,7 +23,20 @@ function Footer() {
                 <SkipNextIcon className="footer__icon" />
                 <RepeatIcon className="footer__green" />
             </div>
-            <div className="footer__right">{/* Volume controller */}3</div>
+            <div className="footer__right">
+                {/* Volume controller */}
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider aria-labelledby="continuous-slider" />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
