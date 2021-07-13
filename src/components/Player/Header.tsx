59897ba6f820a78467d1cf0b8b8b3b33978ffcc3
dinyardin <dinyardin@gmail.com>
2021-07-13 16:17:10 -0400

    Displayed spotify user avatar and name

--- src/components/Player/Header.tsx
+++ src/components/Player/Header.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search'
 import { Avatar } from '@material-ui/core'
+import { useAppContextValue } from '../../services/store/AppContext'
 
 interface Prop {
     spotify: any
 }
 function Header(props: Prop) {
+    const [state, dispatch] = useAppContextValue()
+
     return (
         <div className="header">
             <div className="header__left">
@@ -17,8 +20,11 @@ function Header(props: Prop) {
                 />
             </div>
             <div className="header__right">
-                <Avatar />
-                <h4>Name</h4>
+                <Avatar
+                    src={state.user?.images[0]?.url}
+                    alt={state.user?.display_name}
+                />
+                <h4>{state.user?.display_name}</h4>
             </div>
         </div>
     )
