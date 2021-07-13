7023d25c5f6ec9028ab70e147b40976c5ba6306d
dinyardin <dinyardin@gmail.com>
2021-07-13 12:52:30 -0400

    Added SidebarOption component with stylings and rendered inside Sidebar component

--- src/components/Player/Sidebar.tsx
+++ src/components/Player/Sidebar.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarOption from './SidebarOption'
+import HomeIcon from '@material-ui/icons/Home'
+import SearchIcon from '@material-ui/icons/Search'
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic'
 
 function SideBar() {
-    return <div className="sidebar">hey!</div>
+    return (
+        <div className="sidebar">
+            <img
+                className="sidebar__logo"
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+                alt=""
+            />
+
+            <SidebarOption title="Home" Icon={HomeIcon} />
+            <SidebarOption title="Search" Icon={SearchIcon} />
+            <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr />
+
+            <SidebarOption title="Rock" />
+            <SidebarOption title="R&B" />
+            <SidebarOption title="Classic" />
+        </div>
+    )
 }
 
 export default SideBar
